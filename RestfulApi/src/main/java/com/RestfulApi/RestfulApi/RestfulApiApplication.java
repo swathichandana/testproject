d7465a9539e7485262460d8071506a7e96d988d0
user <user@DESKTOP-2HHS478>
2021-08-02 13:18:07 +0530

    first commit

---
+++ RestfulApi/src/main/java/com/RestfulApi/RestfulApi/RestfulApiApplication.java
@@ -0,0 +1,16 @@
+package com.RestfulApi.RestfulApi;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+}
---
+++ RestfulApi/src/main/java/com/RestfulApi/RestfulApi/models/PostRequest.java
@@ -0,0 +1,19 @@
+package com.RestfulApi.RestfulApi.models;
+
+public class PostRequest {
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
